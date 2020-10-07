267ab115c433ed747f75d9c24832be61ddd9bd2b
Frank Vargas <frankv427@outlook.com>
2020-10-06 20:03:10 -0500

    Para las chicas  parte 2 :)
    Signed-off-by: Frank Vargas <frankv427@outlook.com>

--- app/src/main/java/com/henktech/maskup/MainActivity.java
+++ app/src/main/java/com/henktech/maskup/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // hola chicas!!
 
-        // hola chicas!
+        String str = "hola chicas!";
     }
 }
